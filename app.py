5f1d37edc809c729fd6c94c3f4078a76fae7d3e1
maxencecolmant <maxence.colmant@ynov.com>
2019-09-22 18:47:24 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    str = "value1", "value2", "value3", "value4", "value5", "value6"
+    return jsonify(str)
+
+
+@app.route('/hello2', methods=['GET'])
+def hello2():
+    return 'hello2'
+
+
+app.run(debug=True, host='0.0.0.0')
